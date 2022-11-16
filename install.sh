480519bd28f785101ee0b6029a2bb4e4eead40d3
Mark H. Spatz <mspatz@leaflabs.com>
2022-11-16 14:24:24 -0500

    fix locale.gen uncomment

--- install.sh
+++ install.sh
@@ -58,7 +58,7 @@ pacman --noconfirm -Syyu
 ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime
 hwclock --systohc
 
-sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen
+sed -i '/en_US.UTF-8/s/^#//g' /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 
